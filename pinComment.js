7de734204de9e6cc804c5dc18e94a680522f7c53
chanijung <1016chani@kaist.ac.kr>
2020-11-16 00:16:06 +0900

    create text input on mouse double click

--- pinComment.js
+++ pinComment.js
@@ -11,13 +11,32 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-document.addEventListener("click", myFunction, false);
+// var canvas = document.getElementById("canvas");
+// var context = canvas.getContext("2d");
+
+document.addEventListener("dblclick", myFunction, false);
+// var click_counter = 0;
 
 function myFunction(x){
-    var positionLeft = x.clientX;
-    var positionTop = x.clientY;
-    context.fillStyle = "#000000";
-    context.beginPath();
-    context.arc(positionLeft, positionTop, 50, 0, 2*Math.PI);
-    context.fill();
+    var positionLeft = x.pageX;
+    var positionTop = x.pageY;
+    console.log(positionLeft, positionTop);
+
+    var comment = document.createElement("input",{type:"text"});
+    // comment.type = "text";
+    // click_counter += 1;
+    // comment.id = "comment" + click_counter.toString();
+    comment.style.position = "absolute";
+    document.body.appendChild(comment);
+    comment.style.left = positionLeft+'px';
+    comment.style.top = positionTop+'px';
+    console.log(comment.style.left)
+
+    .addEventListener("keyup", function(event) {
+        if (event.key === "Enter") {
+            // Do work
+        }
+    });
+
 }
+
