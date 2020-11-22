cf6201f19ee01e9e610a8c96caa91cc1cbb8a535
Chani Jung <46154572+chanijung@users.noreply.github.com>
2020-11-23 03:33:57 +0900

    Added user profile, name, timestamp of comments / Changed DB structure to include threads

--- pinComment.js
+++ pinComment.js
@@ -11,45 +11,100 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-var studentId = getParameterByName('studentId');
 var groupId = getParameterByName('groupId');
-console.log("gid: ",groupId);
+var studentId = getParameterByName('studentId');
 
+var myName;
+var friendName;
+var friendId;
+const db = firebase.database().ref('/' + groupId);
+db.child('friends').once('value').then(function(snapshot){
+    snapshot.forEach(element => {
+        const id = element.key;
+        if (id == studentId) {
+            myName = element.child("name").val();
+        } else {
+            friendName = element.child("name").val();
+            friendId = id;
+        }
+    });
+});
 
 //Showing previous comments
 var oldComments = [];
 firebase.database().ref(groupId+'/messenger/').on('value', function(snapshot) {
+    console.log("on value");
     // remove elements iterating through oldComments
-    fLen = oldComments.length;
-    for (i = 0; i < fLen; i++) {
+    oldLen = oldComments.length;
+    for (i = 0; i < oldLen; i++) {
         oldComments[i].remove();
     }
     //clear oldComments
     oldComments = [];
-    console.log("on value")
     snapshot.forEach(function(childSnapshot) {
+        //For each comment
         if (childSnapshot.child("type").val() == "comment"){
-            if (childSnapshot.child("author").val() == studentId){
-                var comment_div = create_comment(childSnapshot.child("left").val(), childSnapshot.child("top").val());
-                comment_div.childNodes.item("comment").readOnly = true;
-                comment_div.childNodes.item("comment").value = childSnapshot.child("string").val();
-                comment_div.setAttribute("key", childSnapshot.key);
-                oldComments.push(comment_div);
-            }
-            else{
-                var fcomment = document.createElement("input",{type:"text"});
-                fcomment.readOnly = true;
-                fcomment.value = childSnapshot.child("string").val();
-                fcomment.style.position = "absolute";
-                fcomment.style.left = childSnapshot.child("left").val()+'px';
-                fcomment.style.top = childSnapshot.child("top").val()+'px';
-                document.body.appendChild(fcomment);
-                oldComments.push(fcomment);
-                console.log(fcomment.value);
-            }
+            var comment = document.createElement("div");
+            oldComments.push(comment);
+            comment.className = "pinComment";
+            comment.style.position = "absolute";
+            comment.style.left = childSnapshot.child("left").val()-600+window.innerWidth/2+'px';
+            comment.style.top = childSnapshot.child("top").val()+'px';
+            comment.setAttribute("commentKey", childSnapshot.key);
+            document.body.appendChild(comment);
+
+            //For each comment thread
+            childSnapshot.child('thread').forEach(function(childSnapshot) {
+                comment.setAttribute("threadKey", childSnapshot.key);
+                var old_thread = create_old_thread();
+                comment.appendChild(old_thread);
+                var sender = childSnapshot.child("sender").val(); //20170610
+                console.log("sender: ",sender);
+                // console.log(firebase.database().ref(groupId+'/friends/'+sender).child("name"));
+                // var sender_name = firebase.database().ref(groupId+'/friends/'+sender).child("name").val;
+                var sender_name;
+                firebase.database().ref(groupId+'/friends/'+sender).child("name").once('value').then(function(snapshot){
+                    sender_name = snapshot.val();
+                    // console.log(snapshot.val());
+                }).then(function() {
+                    console.log("sender name: ",sender_name);
+                    old_thread.getElementsByClassName("pinComment_author_name")[0].innerHTML = sender_name;
+                    old_thread.getElementsByClassName("pinComment_time")[0].innerHTML = childSnapshot.child("time").val();
+                    old_thread.getElementsByClassName("pinComment_author_profile")[0].src = "src/"+sender_name+"_profile.png";
+                    old_thread.getElementsByClassName("pinComment_text_readOnly")[0].readOnly = true;
+                    old_thread.getElementsByClassName("pinComment_text_readOnly")[0].value = childSnapshot.child("string").val();
+                    var old_thread_edit = old_thread.getElementsByClassName("pinComment_edit")[0];
+                    var old_thread_delete = old_thread.getElementsByClassName("pinComment_delete")[0];
+                    if (childSnapshot.child("sender").val() == studentId){
+                        // var pinComment_edit = pinComment_thread.getElementsByClassName("pinComment_edit")[0];
+                        var old_text_readOnly = old_thread.getElementsByClassName("pinComment_text_readOnly")[0]; 
+                        old_thread.getElementsByClassName("pinComment_text_readOnly")[0].addEventListener("keyup", function(event) {
+                            if (event.key === "Enter") {
+                                //When modified the comment
+                                // console.log(firebase.database().ref(groupId+'/messenger/'+comment.getAttribute("commentKey")+"/thread/" + comment.getAttribute("threadKey")).child("string").val);
+                                firebase.database().ref(groupId+'/messenger/'+comment.getAttribute("commentKey")+"/thread/" + comment.getAttribute("threadKey")).child("string").set(old_text_readOnly.value); //child.set로 해야되나?
+                                console.log("modified");
+                                // comment.readOnly = true;
+                                comment.remove();
+                            }
+                        });
+                        old_thread_edit.onclick = function(){
+                            old_thread.getElementsByClassName("pinComment_text_readOnly")[0].readOnly = false;
+                        };
+                        old_thread_delete.onclick = function(){
+                            firebase.database().ref(groupId+'/messenger/').child(comment.getAttribute("commentKey")).remove();
+                            comment.remove();
+                        };
+                    }
+                    else{
+                        old_thread_edit.remove();
+                        old_thread_delete.remove();
+                    }
+                })
+
+            });
         }
     });
-    // console.log("oldComments ",oldComments.length);
 
 });
 
@@ -82,72 +137,95 @@ function create_comment_at_event(e){
 }
 
 function create_comment(positionLeft, positionTop){
-    // console.log("create comment", positionLeft, positionTop);
-    // var positionLeft = x.pageX;
-    // var positionTop = x.pageY;
 
-    var comment_div = document.createElement("div");
-    document.body.appendChild(comment_div);
-    comment_div.style.position = "absolute";
-    comment_div.style.left = positionLeft+'px';
-    comment_div.style.top = positionTop+'px';
-    // comment_div.zIndex = 10;
+    var comment = document.createElement("div");
+    comment.className = "pinComment";
+    document.body.appendChild(comment);
+    comment.style.position = "absolute";
+    comment.style.left = positionLeft+'px';
+    // comment.style.left = 600 + (positionLeft - window.innerWidth/2);
+    comment.style.top = positionTop+'px';
+    comment.setAttribute("commentKey","none");//
+    comment.innerHTML = '<div class = "pinComment_thread"><div class = "pinComment_thread_header"> <img class = "pinComment_author_profile"> <div class = "pinComment_author_info"><div class = "pinComment_author_name"></div><div class = "pinComment_time"></div></div><img class = "pinComment_delete" src = "src/delete.png"></div><div class = "pinComment_input"><input class = "pinComment_text_input"><input type="submit" class="pinComment_text_input_enter" value="comment"></div></div></div>';
+    comment.getElementsByClassName("pinComment_author_profile")[0].src = "src/"+myName+"_profile.png";
+    comment.getElementsByClassName("pinComment_author_name")[0].innerHTML = myName;//
 
-    var comment = document.createElement("input",{type:"text"});
-    comment_div.appendChild(comment);
-    comment.className = "comment_div";
-    comment.id = "comment";
-    comment_div.setAttribute("key","none");
+    var today = new Date();
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    comment.getElementsByClassName("pinComment_time")[0].innerHTML = today.getHours() + ":" + today.getMinutes() + " " + months[today.getMonth()] + " " + today.getDate();
 
-    var modify = document.createElement("img");
-    modify.className = "comment_div";
-    comment_div.appendChild(modify);
-    modify.src = "src/modify.png";
-    modify.width = comment.offsetHeight;
-    modify.height = comment.offsetHeight;
-    modify.onclick = function(){
-        comment.readOnly = false;
-    };
-
-    var deletion = document.createElement("img");
-    deletion.className = "comment_div";
-    comment_div.appendChild(deletion);
-    deletion.src = "src/delete.png";
-    deletion.width = comment.offsetHeight;
-    deletion.height = comment.offsetHeight;
-    deletion.onclick = function(){
-        console.log(comment_div.getAttribute("key"));
-        firebase.database().ref(groupId+'/messenger/').child(comment_div.getAttribute("key")).remove();
-        comment_div.remove();
-    };
-
-    // var commentKey = 0;
-    comment.addEventListener("keyup", function(event) {
-        if (event.key === "Enter") {
-            if (comment_div.getAttribute("key") === "none"){
-                //When first entered the comment
-                var commentKey = firebase.database().ref(groupId+'/messenger/').push({
-                    type: "comment",
-                    author: studentId,
-                    time: Math.floor(Date.now() / 1000),
-                    string: comment.value,
-                    left: positionLeft,
-                    top: positionTop,
-                }).key;
-                comment_div.setAttribute("key", commentKey);
-                console.log("Added ",comment_div.getAttribute("key"));
-            }
-            else{
-                //When modified the comment
-                firebase.database().ref(groupId+'/messenger/'+comment_div.getAttribute("key")+"/").child("string").set(comment.value);
-                console.log("modified");
-            }
-            comment.readOnly = true;
-            comment_div.remove();
-        }
+    var comment_text_input = comment.getElementsByClassName("pinComment_text_input")[0];
+    comment_text_input.id = "pinComment_text_input";//
+    // comment_text_input.addEventListener("keyup", send_comment, false);
+    comment_text_input.addEventListener("keyup", function(event) {
+        if (event.key === "Enter") {send_comment();}
     });
-    return comment_div;
+
+    var enter = comment.getElementsByClassName("pinComment_text_input_enter")[0];
+    enter.onclick = send_comment;
+    // enter.addEventListener("click", send_comment, false);
+    // console.log(comment.getElementsByClassName("pinComment_delete"));
+    comment.getElementsByClassName("pinComment_delete")[0].onclick = function(){
+        // console.log(comment_thread.getAttribute("key"));
+        console.log(firebase.database().ref(groupId+'/messenger/'+comment.getAttribute("commentKkey")));
+        firebase.database().ref(groupId+'/messenger/'+comment.getAttribute("commentKkey")).remove();
+        // firebase.database().ref(groupId+'/messenger/').child(comment_thread.getAttribute("key")).remove();
+        comment.remove();
+    };
+
+    function send_comment(){
+        console.log("send comment");
+        // if (event.key === "Enter") {
+        if (comment.getAttribute("commentKey") === "none"){
+            //When first entered the comment
+            var commentKey = firebase.database().ref(groupId+'/messenger/').push({
+                type: "comment",
+                left: 600 + (positionLeft - window.innerWidth/2),
+                top: positionTop,
+                thread: "none"
+            }).key;
+            var threadKey = firebase.database().ref(groupId+'/messenger/'+commentKey+'/thread/').push({
+                sender: studentId,
+                time: today.getHours() + ":" + today.getMinutes() + " " + months[today.getMonth()] + " " + today.getDate(),
+                string: comment_text_input.value
+            }).key;
+            comment.setAttribute("commentKey", commentKey);
+            comment.setAttribute("threadKey", threadKey);
+            console.log("Added ",comment.getAttribute("commentKey"), comment.getAttribute("threadKey", threadKey));
+        }
+        else{
+            //When modified the comment
+            console.log(firebase.database().ref(groupId+'/messenger/'+comment.getAttribute("commentKey")+"/thread/" + comment.getAttribute("threadKey")).child("string").val);
+            firebase.database().ref(groupId+'/messenger/'+comment.getAttribute("commentKey")+"/thread/" + comment.getAttribute("threadKey")).child("string").set(comment_text_input.value); //child.set로 해야되나?
+            console.log("modified");
+        }
+        // comment.readOnly = true;
+        comment.remove();
+        // }
+    }
+//
+    return comment;
 }
+
+
+
+function create_old_thread(){
+    var pinComment_thread = document.createElement("div");
+    pinComment_thread.className = "pinComment_thread";
+    pinComment_thread.innerHTML = '<div class = "pinComment_thread_header"><img class = "pinComment_author_profile"><div class = "pinComment_author_info"><div class = "pinComment_author_name"></div><div class = "pinComment_time"></div></div><img class = "pinComment_edit" src = "src/modify.png"><img class = "pinComment_delete" src = "src/delete.png"></div><div class = "pinComment_input"><input type="text" class = "pinComment_text_readOnly"></div></div>';
+
+//
+    // var comment_text_input = comment.getElementsByClassName("pinComment_text_input")[0];
+    // comment_text_input.id = "pinComment_text_input";//
+    // comment_text_input.addEventListener("keyup", function(event) {
+    //     if (event.key === "Enter") {send_comment(null);}
+    // });
+
+    //
+//
+    return pinComment_thread;
+}
+
 
 
 function getParameterByName(name) {
@@ -156,3 +234,41 @@ function getParameterByName(name) {
         results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
+
+
+
+
+
+/*Modify
+    var modify = document.createElement("img");
+    modify.className = "pinComment_thread";
+    comment_thread.appendChild(modify);
+    modify.src = "src/modify.png";
+    modify.width = comment.offsetHeight;
+    modify.height = comment.offsetHeight;
+    //
+    modify.onclick = function(){
+        comment.readOnly = false;
+    };
+    //
+
+    var enter = document.createElement("input",{type:"submit"});
+    enter.value
+    enter.className = "pinComment_text_input_enter";
+    comment_thread.appendChild(enter);
+*/
+
+    
+/*Delete
+    var deletion = document.createElement("img");
+    deletion.className = "pinComment_thread";
+    comment_thread.appendChild(deletion);
+    deletion.src = "src/delete.png";
+    deletion.width = comment.offsetHeight;
+    deletion.height = comment.offsetHeight;
+    deletion.onclick = function(){
+        // console.log(comment_thread.getAttribute("key"));
+        firebase.database().ref(groupId+'/messenger/').child(comment_thread.getAttribute("key")).remove();
+        comment_thread.remove();
+    };
+*/
