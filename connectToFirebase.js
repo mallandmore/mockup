daca0613374160dae761cf0b6a75c1929ea29451
ooooooseok@gmail.com <62334707+gh-ooooseok@users.noreply.github.com>
2020-12-09 23:50:56 +0900

    Revert "threejs"
    This reverts commit 94f6c83e4561e4dd77ca8bd16ab7e83b05c10a42.

--- connectToFirebase.js
+++ connectToFirebase.js
@@ -10,21 +10,3 @@ var firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-
-var init = firebase.initializeApp({
-    apiKey: "만든키",
-    authDomain: "만든도메인",
-    databaseURL: "만든주소",
-    projectId: "아이디0",
-    storageBucket: "만든버킷저장소",
-    messagingSenderId: "아이디1",
-    appId: "아이디2",
-    measurementId: "아이디3"
-});
-
-var db = init.firestore();  //위 설정대로 저장소에 접속합니다.
-let ref = db.collection('board');  //내가 사용할 컬렉션입니다.
